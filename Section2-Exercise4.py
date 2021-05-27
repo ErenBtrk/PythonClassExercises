c8e145752ecf44decac28a166949d3e667310f7a
= <=>
2021-05-28 00:00:33 +0300

    Section2-Exercise 4

--- Section2-Exercise4.py
+++ Section2-Exercise4.py
@@ -6,13 +6,35 @@ Output : [[], [6], [5], [5, 6], [4], [4, 6], [4, 5], [4, 5, 6]]
 
 '''
 
-class py_solution:
-    def sub_sets(self, sset):
-        return self.subsetsRecur([], sorted(sset))
+# class py_solution:
+#     def sub_sets(self, sset):
+#         return self.subsetsRecur([], sorted(sset))
     
-    def subsetsRecur(self, current, sset):
-        if sset:
-            return self.subsetsRecur(current, sset[1:]) + self.subsetsRecur(current + [sset[0]], sset[1:])
-        return [current]
+#     def subsetsRecur(self, current, sset):
+#         if sset:
+#             return self.subsetsRecur(current, sset[1:]) + self.subsetsRecur(current + [sset[0]], sset[1:])
+#         return [current]
 
-print(py_solution().sub_sets([4,5,6]))
+# print(py_solution().sub_sets([4,5,6]))
+
+
+#####################################################################################################
+
+from itertools import combinations
+
+class py_solution2:
+    def sub_sets(self,listArg):
+        resultList = []
+        i = 0
+        while i <= len(listArg):
+            comb = combinations(listArg,i)
+            for item in comb:
+                resultList.append(item)
+            i += 1
+        return resultList
+        
+    
+
+list1 = [4, 5, 6]
+
+print(py_solution2().sub_sets(list1))
