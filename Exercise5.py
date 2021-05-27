ebf56451447dc82240e497e5ff1f130ce607798d
= <=>
2021-05-27 21:37:06 +0300

    Exercise 5

---
+++ Exercise5.py
@@ -0,0 +1,10 @@
+'''
+5. Define a Python function student(). Using function attributes display the names of all arguments.
+
+'''
+
+def student(firstName,lastName,age):
+    return f"{firstName},{lastName},{age}"
+
+
+print(student("Ali","Veli","25"))
