db53d7346eae75073f8452b94b459da7c81dda88
= <=>
2021-05-28 21:56:48 +0300

    Section2-Exercise 12

---
+++ Section2-Exercise12.py
@@ -0,0 +1,18 @@
+'''
+12. Write a Python program to get the class name of an instance in Python.
+'''
+
+from typing import Type
+
+
+class Student:
+    pass
+
+class Teacher:
+    pass
+
+s1 = Student()
+t1 = Teacher()
+
+print(type(s1).__name__)
+print(type(t1).__name__)
